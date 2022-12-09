5444ce04b8a2848eb5167cfe5f8cfa25514b8ab9
Alessandro Ontani <casualotn@gmail.com>
2022-12-09 12:51:31 +0100

    completed exercises 5 to 10

--- D5_v2/D5.js
+++ D5_v2/D5.js
@@ -81,33 +81,93 @@ console.log(cars);
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
-console.log("------------------------------ EX. N. 1 ------------------------------");
+console.log("------------------------ EX. N. 6 first part -------------------------");
 
+cars.push({
+  brand: "Toyota",
+  model: "Yaris",
+  color: "gray",
+  trims: ["hybrid", "X-F1", "nice-car"],
+  licenseplate: "FG 567 TN",
+});
+
+console.log(cars);
+console.log("------------------------ EX. N. 6 second part -------------------------");
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+
+console.log(cars);
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-console.log("------------------------------ EX. N. 1 ------------------------------");
+console.log("------------------------------ EX. N. 7 ------------------------------");
 
 const justTrims = [];
 
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]);
+}
+
+console.log(justTrims);
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
-console.log("------------------------------ EX. N. 1 ------------------------------");
+console.log("------------------------------ EX. N. 8 ------------------------------");
+for (let i = 0; i < cars.length; i++) {
+  const carColor = cars[i].color.split("");
+  if (carColor[0] === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
-console.log("------------------------------ EX. N. 1 ------------------------------");
+console.log("------------------------------ EX. N. 9 ------------------------------");
 
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+let i = 0;
+
+while (numericArray[i] !== 32) {
+  console.log(numericArray[i]);
+  i++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-console.log("------------------------------ EX. N. 1 ------------------------------");
+console.log("------------------------------ EX. N. 10 ------------------------------");
 
 const charactersArray = ["g", "n", "u", "z", "d"];
+const numericCharactersArray = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+  const element = charactersArray[i];
+  switch (element) {
+    case "g":
+      numericCharactersArray.push(7);
+      break;
+    case "n":
+      numericCharactersArray.push(12);
+      break;
+    case "u":
+      numericCharactersArray.push(19);
+      break;
+    case "z":
+      numericCharactersArray.push(21);
+      break;
+    case "d":
+      numericCharactersArray.push(4);
+      break;
+  }
+}
+
+console.log(charactersArray);
+console.log(numericCharactersArray);
